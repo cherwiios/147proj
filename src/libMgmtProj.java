a59987d0d1349173baebafa8c23a573077a38682
donnar1 <donnar1@umbc.edu>
2024-12-02 14:36:28 -0500

    Update libMgmtProj.java

--- src/libMgmtProj.java
+++ src/libMgmtProj.java
@@ -4,3 +4,40 @@ public class libMgmtProj {
     
   }
 }
+
+
+import java.util.Date;
+
+public class Book {
+    private String title;
+    private String author;
+    private int bookId;
+    private static int bookCounter = 0;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.bookId = (int)(Math.random() * 10000); // Use Math.random() to assign a random book ID
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public static int getBookCounter() {
+        return bookCounter;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId + ", Title: " + title + ", Author: " + author;
+    }
+}
