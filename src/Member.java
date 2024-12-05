3469d45cf8b5b7290ecac0eb3451298d2b83a39a
salmans803 <salmans1@umbc.edu>
2024-12-04 21:24:18 -0500

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,13 +1,19 @@
+// Class definition for Member
 public class Member {
+    // Private fields for member's name and ID
     private String name;
     private int memberId;
+
+    // Static field to keep track of the number of members
     private static int memberCounter = 0;
 
+    // Constructor to initialize the Member object with a name
     public Member(String name) {
-        this.name = name;
-        this.memberId = ++memberCounter;
+        this.name = name; // Assigns the name passed to the constructor
+        this.memberId = ++memberCounter; // Increments memberCounter and assigns it to memberId
     }
 
+    // Method to display information about the member
     public void displayInfo() {
         System.out.println("Member ID: " + memberId + ", Name: " + name);
     }
