221731c7cdf3c520a782168a790cb8072908470c
salmans803 <salmans1@umbc.edu>
2024-12-08 15:50:11 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * Main class to run the library management system.
+ */
 public class Main {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the standard input stream
         Scanner scanner = new Scanner(System.in);
 
-        // Greeting message for the user
-        System.out.println("Welcome to the Library Management System!");
-        System.out.println("Press Enter to start...");
+        System.out.println("Press Enter to begin...");
 
-        // Waits for the user to press Enter to proceed
         scanner.nextLine();
 
-        // Create an instance of LibrarySystem and start the system
         LibrarySystem librarySystem = new LibrarySystem();
-        librarySystem.start(); // Start the library system which is likely to handle further interactions
+        librarySystem.start();
     }
 }
+
